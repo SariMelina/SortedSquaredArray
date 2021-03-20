7e102d0577e4485c6a5282d60dce13ed6202fedc
SariMelina <mmkings_beba@hotmail.com>
2021-03-20 08:01:12 -0600

    Second commit

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ function sortedSquaredArray(array) {
   })
   return newArray;
 }
-
-const array = [1,2,3,5,6,8,9]
-console.log(`[${sortedSquaredArray(array)}]`);
-
 // Do not edit the line below.
 exports.sortedSquaredArray = sortedSquaredArray;
