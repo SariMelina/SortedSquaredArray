9d557e5ca855457313cd56a72cd9c02d84a876cd
SariMelina <mmkings_beba@hotmail.com>
2021-03-20 08:00:45 -0600

    First commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function sortedSquaredArray(array) {
-  // Write your code here.
-  return [];
+  const newArray = array.map((x) => {
+    return x * x
+  })
+  return newArray;
 }
+
+const array = [1,2,3,5,6,8,9]
+console.log(`[${sortedSquaredArray(array)}]`);
 
 // Do not edit the line below.
 exports.sortedSquaredArray = sortedSquaredArray;
